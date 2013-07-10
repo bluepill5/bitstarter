cd1626a6e7f243dcd7bb627d9e54800960788b11
bluepill5 <bluepills89@gmail.com>
2013-07-10 12:57:59 -0500

    New features

--- grader.js
+++ grader.js
@@ -21,6 +21,8 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
@@ -28,6 +30,7 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
+    console.log(infile.toString());
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
@@ -65,10 +68,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Command line defined URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url != null) {
+      var urlAsString = program.url.toString();
+      rest.get(urlAsString).on('complete', function(result) {
+        if (result instanceof Error) {
+          util.puts('Error: ' + result.message);
+          this.retry(5000); // try again after 5 sec
+        } else {
+            fs.writeFileSync('outfile.html', result);
+            var checkJson = checkHtmlFile('outfile.html', program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+          }
+      });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } 
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
